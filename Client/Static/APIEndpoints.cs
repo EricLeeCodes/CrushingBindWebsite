6c22ef865a7b8053efe9a31caae25b874975e39b
Eric Lee <ericleecodes@gmail.com>
2022-04-13 10:39:58 -0500

    Updated API Endpoints

--- Client/Static/APIEndpoints.cs
+++ Client/Static/APIEndpoints.cs
@@ -5,7 +5,7 @@
 #if DEBUG
         internal const string ServerBaseUrl = "https://localhost:7106";
 #else
-        internal const string ServerBaseUrl = "https://appname.azurewebsites.net";
+        internal const string ServerBaseUrl = "https://crushingbindserver.azurewebsites.net";
 #endif
         internal readonly static string s_archiveModels = $"{ServerBaseUrl}/api/archivemodels";
     }

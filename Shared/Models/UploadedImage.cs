a4d5cf8a948bbf10ae73f6ac4675dc7cf65ed0d5
Eric Lee <ericleecodes@gmail.com>
2022-04-21 00:37:18 -0500

    Started on uploading images for admin controls

---
+++ Shared/Models/UploadedImage.cs
@@ -0,0 +1,16 @@
+﻿namespace Shared.Models
+{
+    public class UploadedImage
+    {
+        //Blobs = binary large objects
+
+        public string NewImageFileExtension { get; set; }
+
+        //Base64 is a string that represents binary
+        public string NewImageBase64Content { get; set; }
+        public string OldImagePath { get; set; }
+
+
+
+    }
+}
